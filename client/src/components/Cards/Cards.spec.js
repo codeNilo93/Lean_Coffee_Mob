55afd1fe8976c2a690f6d3e801d50d3c6fd6ee49
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-01 10:53:07 +0100

    Rebuild tests

--- client/src/components/Cards/Cards.spec.js
+++ client/src/components/Cards/Cards.spec.js
@@ -1,31 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import Card from '../Card/Card'
+import Cards from './Cards'
 
 test('loads item', () => {
-  const sampleCards = [
-    { text: 'What is node?', author: 'Stefan Bering' },
-    { text: 'What is React?', author: ' Timur Buchberger' },
-    { text: 'What is styled components?', author: ' Danilo Bräuer' },
-  ]
+  render(<Cards />)
+  const mainItem = screen.getByRole('main')
+  expect(mainItem).toBeInTheDocument()
 
-  render(
-    <main>
-      {sampleCards.map(card => {
-        return (
-          <Card
-            text={card.text.trim()}
-            author={card.author.trim()}
-            key={card.text.trim()}
-          />
-        )
-      })}
-    </main>
-  )
-
-  for (let sample of sampleCards) {
-    const itemText = screen.getByText(sample.text.trim())
-    expect(itemText).toBeInTheDocument()
-    const authorText = screen.getByText(sample.author.trim())
-    expect(authorText).toBeInTheDocument()
-  }
+  const textItems = screen.getAllByRole('heading')
+  expect(textItems).toHaveLength(3)
 })
