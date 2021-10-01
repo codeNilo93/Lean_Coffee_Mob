096e3672d7423e363a34da4dfa12316939cc5257
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-01 14:11:52 +0100

    Add test for onSubmit functionality

--- client/src/components/Form/Form.spec.js
+++ client/src/components/Form/Form.spec.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Form from './Form'
 
+const mockOnSubmit = jest.fn()
+
 test('loads item', () => {
-  render(<Form />)
+  render(<Form onAddQuestion={mockOnSubmit} />)
   const inputItem = screen.getByPlaceholderText('Please enter your question')
   expect(inputItem).toBeInTheDocument()
 
@@ -11,4 +14,12 @@ test('loads item', () => {
 
   const buttonItem = screen.getByText('Save')
   expect(buttonItem).toBeInTheDocument()
+})
+
+test('button has onClick functionality', () => {
+  render(<Form onAddQuestion={mockOnSubmit} />)
+
+  const buttonItem = screen.getByText('Save')
+  userEvent.click(buttonItem)
+  expect(mockOnSubmit).toHaveBeenCalled()
 })
