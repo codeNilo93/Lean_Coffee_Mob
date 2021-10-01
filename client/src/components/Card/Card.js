8f8cc164930525e265a5dfe50da75e220d6a08c3
StefanBehring <chadhz@gmx.de>
2021-10-01 15:40:19 +0200

    Add deleteButton

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,13 +1,28 @@
 import styled from 'styled-components/macro'
 
-const Card = ({ text, author }) => {
+const Card = ({ id, text, author, onDeleteQuestion }) => {
+  const deleteQuestionHandler = () => {
+    onDeleteQuestion(id)
+  }
+
   return (
     <StyledSection>
+      <StyledButton onClick={deleteQuestionHandler}>
+        Fuck this shit
+      </StyledButton>
       <h2>{text}</h2>
       <p>{author}</p>
     </StyledSection>
   )
 }
+
+const StyledButton = styled.button`
+  background: #f44336;
+  color: #fff;
+  border: 1px solid #000;
+  border-radius: 25px;
+`
+
 const StyledSection = styled.section`
   width: 320px;
   padding: 1rem;
