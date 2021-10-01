79361e936ef1124df092a8e32f43406358812225
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-01 14:11:26 +0100

    Change test for Cards

--- client/src/components/Cards/Cards.spec.js
+++ client/src/components/Cards/Cards.spec.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import Cards from './Cards'
 
+const testCards = [
+  { text: 'What is node?', author: 'Stefan Bering', id: 123 },
+  { text: 'What is React?', author: ' Timur Buchberger', id: 124 },
+  { text: 'What is styled components?', author: ' Danilo Bräuer', id: 125 },
+]
+
 test('loads item', () => {
-  render(<Cards />)
+  render(<Cards data={testCards} />)
   const mainItem = screen.getByRole('main')
   expect(mainItem).toBeInTheDocument()
 
