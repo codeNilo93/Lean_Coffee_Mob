6885cc96c7d0ae5844a999650d4b2b25dd67550a
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-01 14:12:20 +0100

    Add ability to store Cards in Localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import styled from 'styled-components/macro'
 import Cards from './components/Cards/Cards'
 import Form from './components/Form/Form'
-import localStorageInit from './components/Cards/LocalStorageInit'
+import localStorageInit from './components/LocalStorage/LocalStorageInit'
 import { useState } from 'react'
 import { nanoid } from 'nanoid'
 
-
-
 function App() {
   localStorageInit()
-  const [data, setData] = useState(JSON.parse(localStorage.getItem("data")))
+  const [data, setData] = useState(JSON.parse(localStorage.getItem('data')))
   const addQuestionHandler = question => {
-    const newData = [...data, {text: question, author: "user", id: nanoid()}]
-    localStorage.setItem("data", JSON.stringify(newData))
+    const newData = [...data, { text: question, author: 'user', id: nanoid() }]
+    localStorage.setItem('data', JSON.stringify(newData))
     setData(newData)
   }
 
   return (
     <StyledDiv>
-      <Cards data={data}/>
+      <Cards data={data} />
       <Form onAddQuestion={addQuestionHandler} />
     </StyledDiv>
   )
