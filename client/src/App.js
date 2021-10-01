65571a6f934a71cc0c1124dbf4b957df9f3a5d35
StefanBehring <chadhz@gmx.de>
2021-10-01 12:40:23 +0200

    Add styled component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
+import styled from 'styled-components/macro'
 import Cards from './components/Cards/Cards'
 import Form from './components/Form/Form'
 
 function App() {
+  const addQuestionHandler = question => {
+    alert('Question: ' + question)
+  }
+
   return (
-    <div className="App">
+    <StyledDiv>
       <Cards />
-      <Form />
-    </div>
+      <Form onAddQuestion={addQuestionHandler} />
+    </StyledDiv>
   )
 }
+
+const StyledDiv = styled.div`
+  display: grid;
+  grid-template-rows: auto 40px;
+  text-align: center;
+  height: 100vh;
+`
 
 export default App
